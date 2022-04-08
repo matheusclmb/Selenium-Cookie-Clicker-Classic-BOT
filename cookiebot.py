de9b60cab81e90b1e6afd643cc7708f26281303e
Matheus <ccolombomatheus@gmail.com>
2022-04-08 18:27:54 +0100

    Update cookiebot.py

--- cookiebot.py
+++ cookiebot.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-chrome_driver_path = "/Users/persa/Desktop/programstuff/Development/chromedriver"
+chrome_driver_path = "YOUR CHROME DRIVER LOCATION"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
